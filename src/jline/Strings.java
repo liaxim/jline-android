7fec89037f27d42add452cb3350ed7d66d8d7edd
Danke Xie <d.xie@samsung.com>
2016-01-25 13:27:31 -0800

    Add jline 1.0
    Remove things that don't compile on Android
    Put string resources in class Strings

---
+++ src/jline/Strings.java
@@ -0,0 +1,8 @@
+package jline;
+
+public class Strings {
+    static final String display_candidates = "Display all {0} possibilities? (y or n)";
+    static final String candidates_yes = "y";
+    static final String candidates_no = "n";
+    static final String display_more = "--More--";
+}
