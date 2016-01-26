5cc2e10eda062e984d5b793d660028df6f053d32
Danke Xie <danke.xie@gmail.com>
2016-01-25 22:34:53 -0800

    Add Strings

---
+++ src/jline/Strings.java
@@ -0,0 +1,8 @@
+package jline;
+
+public class Strings {
+    public static final String display_candidates = "Display all {0} possibilities? (y or n)";
+    public static final String candidates_yes = "y";
+    public static final String candidates_no = "n";
+    public static final String display_more = "--More--";
+}
